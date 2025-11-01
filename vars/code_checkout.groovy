f6669c5440ebe7ad85e66205bac33d43fc21f9fa
Sumanta Das <dsumanta31746@gmail.com>
2025-11-02 01:05:11 +0530

    fix:code checkout issue and SonarQube server issue

--- vars/code_checkout.groovy
+++ vars/code_checkout.groovy
@@ -1,4 +1,4 @@
-def call(String gitUrl, String branch) {
+def call(String gitUrl, String branch, String credentialsId) {
     if (!gitUrl) {
         error "Git URL is required"
     }
@@ -6,5 +6,6 @@ def call(String gitUrl, String branch) {
         branch = 'main' 
     }
 
-    git url: "${gitUrl}", branch: "${branch}"
+    git url: "${gitUrl}", branch: "${branch}", credentialsId: "${credentialsId}"
 }
+//  code_checkout("https://github.com/dsumanta/AI-Project-Management.git","dev","github")
